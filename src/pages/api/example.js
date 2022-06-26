2214000b259cb9c38c56f4bc17fc54a68f5b482d
cristian <bizapro123@gmail.com>
2022-06-25 21:27:16 -0500

    Debug para corregir el error 504 en vercel

--- src/pages/api/example.js
+++ src/pages/api/example.js
@@ -2,5 +2,5 @@ import connectMongo from "lib/connectMongo";
 
 export default async (req, res)=> {
 
-  return res.json({mongo: process.MONGO_URL});
+  return res.json({mongo: process.env.MONGO_URL});
 }
